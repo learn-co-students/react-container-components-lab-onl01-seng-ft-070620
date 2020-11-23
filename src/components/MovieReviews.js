c0d28a335dc7277157d6a0e79fee747acef04440
cTraymany <chindalatht@gmail.com>
2020-11-23 10:15:24 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
-// Code MovieReviews Here
+import React from 'react';
+import uuid from 'uuid';
+
+const MovieReviews = (props) => {
+    return (
+        <ul className="review-list">
+            {props.reviews.map(review => <li key={uuid()} className="review">{review.display_title}</li>)}
+        </ul>
+    )
+}
+
+export default MovieReviews
